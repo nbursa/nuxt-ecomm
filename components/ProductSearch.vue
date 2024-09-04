0e5f335345c24ee7078add12be710384bb53aff1
Nenad <nbursa@gmail.com>
2024-09-04 19:37:34 +0200

    Responsive filter layout

--- components/ProductSearch.vue
+++ components/ProductSearch.vue
@@ -1,30 +1,37 @@
 <template>
-  <div class="mb-6">
-    <input
-      type="text"
-      v-model="searchQuery"
-      placeholder="Search products..."
-      class="border border-gray-400 bg-white text-black p-2 rounded w-full mb-4 focus:outline-none"
-      @input="updateFilters"
-    />
+  <div class="">
+    <div
+      class="w-full mb-6 md:flex md:items-center md:justify-evenly md:space-x-6"
+    >
+      <div class="w-full md:w-1/2">
+        <label class="block font-semibold mb-2"> Search </label>
+        <input
+          type="text"
+          v-model="searchQuery"
+          placeholder="Search products..."
+          class="border border-gray-400 bg-white text-black p-2 rounded w-full focus:outline-none"
+          @input="updateFilters"
+        />
+      </div>
 
-    <div class="mb-4">
-      <label class="block font-semibold mb-2"> Filter by Category </label>
-      <select
-        v-model="selectedCategory"
-        @change="updateFilters"
-        class="border border-gray-400 bg-white text-black p-2 rounded w-full mb-4 focus:outline-none"
-      >
-        <option value="">All Categories</option>
-        <option
-          v-for="category in categories"
-          :key="category"
-          :value="category"
-          class="text-black bg-white"
+      <div class="w-full md:w-1/2">
+        <label class="block font-semibold mb-2"> Filter by Category </label>
+        <select
+          v-model="selectedCategory"
+          @change="updateFilters"
+          class="w-full border border-gray-400 bg-white text-black p-2 rounded focus:outline-none"
         >
-          {{ category }}
-        </option>
-      </select>
+          <option value="">All Categories</option>
+          <option
+            v-for="category in categories"
+            :key="category"
+            :value="category"
+            class="text-black bg-white"
+          >
+            {{ category }}
+          </option>
+        </select>
+      </div>
     </div>
 
     <div>
